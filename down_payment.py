dccae8053a05832aa46afbd8b73255f9f3508147
Kanyarat Benamad <159973709+GoodiesLavender@users.noreply.github.com>
2025-02-12 16:40:13 -0600

    Add files via upload

---
+++ down_payment.py
@@ -0,0 +1,6 @@
+down_payment = float(input("Enter down payment amount: "))
+total = float(input("Enter the total price: "))
+due = down_payment-total
+print("The due payment amount is ",due)
+
+
---
+++ favoriteColor.py
@@ -0,0 +1,2 @@
+color = input ("Enter your favorite color: ")
+print(color)
